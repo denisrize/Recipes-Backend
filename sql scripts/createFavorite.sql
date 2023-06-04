62c17d1d3a85498f9466f3ca136676886a75b247
liortkach <liortkach23@gmail.com>
2023-06-04 12:29:51 +0300

    Without Head

--- sql scripts/createFavorite.sql
+++ sql scripts/createFavorite.sql
@@ -1,7 +1,7 @@
 CREATE TABLE recipe_instructions (
   user_id INT,
   recipe_id VARCHAR(45),
-  instruciton VARCHAR(500),
+  equipment VARCHAR(45),
   PRIMARY KEY (user_id, recipe_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
